a26ad9d2f20f6873730a6e45c10c2bf81036b150
Trisha Quinto <trishablue588@gmail.com>
2021-11-09 21:21:36 +0800

    index update

--- template/assets/js/progress.js
+++ template/assets/js/progress.js
@@ -74,3 +74,82 @@ $(document).ready(function(){
   })
   
   });
+
+
+ 
+
+//checklist
+
+var checks = document.getElementsByName('checkme');
+var checkBoxList = document.getElementById('checkBoxList');
+var sendbtn = document.getElementById('sendNewSms');
+
+function allTrue(nodeList) {
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].checked === false) return false;
+  }
+  return true;
+}
+
+checkBoxList.addEventListener('click', function(event) {
+  sendbtn.disabled = true;
+  if (allTrue(checks)) sendbtn.disabled = false;
+});
+
+//rate page show hide 1st applicant
+$(document).ready(function(){
+    $(".hide1").click(function(){
+        $(".form1").hide();
+    });
+    $(".show1").click(function(){
+        $(".form1").css('display', 'flex');
+    });
+});
+
+//rate page show hide 2nd applicant 
+
+$(document).ready(function(){
+    $(".hide2").click(function(){
+        $(".form2").hide();
+    });
+    $(".show2").click(function(){
+        $(".form2").css('display', 'flex');
+    });
+});
+
+//rate page show hide 3rd applicant 
+
+$(document).ready(function(){
+    $(".hide3").click(function(){
+        $(".form3").hide();
+    });
+    $(".show3").click(function(){
+        $(".form3").css('display', 'flex');
+    });
+});
+
+//rate page show hide 4th applicant 
+
+$(document).ready(function(){
+    $(".hide4").click(function(){
+        $(".form4").hide();
+    });
+    $(".show4").click(function(){
+        $(".form4").css('display', 'flex');
+    });
+});
+
+//Only 1 check on checkbox
+function onlyOne(checkbox) {
+  var checkboxes = document.getElementsByName('check')
+  checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false
+  })
+}
+function onlyOne(checkbox) {
+  var checkboxes = document.getElementsByName('check1')
+  checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false
+  })
+}
+
